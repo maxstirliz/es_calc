74173ac9fb7d05363265f53c657b26e7d197d8cb
Artem Lymanskyi <artem@laptop.local>
2023-08-04 15:45:55 +0300

    Shopping Item has been tweaked

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,17 @@ import 'package:es_calc/screens/shopping_list_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const ShoppingListScreen());
+  runApp(const EasySoppingCalculator());
+}
+
+class EasySoppingCalculator extends StatelessWidget {
+  const EasySoppingCalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Easy Shopping Calculator',
+      home: ShoppingListScreen(),
+    );
+  }
 }
--- lib/model/shopping_item.dart
+++ lib/model/shopping_item.dart
@@ -7,14 +7,14 @@ class ShoppingItem {
     this.title = 'Product',
     required this.price,
     this.quantity = 1,
-    this.isIncluded = true,
-    this.isBought = false,
+    this.isSummed = true,
+    this.isBought = true,
   }) : id = uuid.v4();
 
   final String id;
   final String title;
   double price;
   double quantity;
-  bool isIncluded;
+  bool isSummed;
   bool isBought;
 }
